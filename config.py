2a21c3323d9d4a885e9f628acc6d2be9299006d9
karan243coder <152502742+karan243coder@users.noreply.github.com>
2025-03-25 12:26:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,7 +51,7 @@ class Config(object):
     TECH_VJ_SESSION_NAME = "Cluster0"
     
     # database uri (mongodb)
-    TECH_VJ_DATABASE_URL = os.environ.get("TECH_VJ_DATABASE_URL", "mongodb+srv://musicworld7243:2aiW9EfvHLILV37g@cluster0.p39dl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    TECH_VJ_DATABASE_URL = os.environ.get("TECH_VJ_DATABASE_URL", "mongodb+srv://appuz:chrijismiappuz@cluster0.yngvhc2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     TECH_VJ_MAX_RESULTS = "50"
 
     # channel information
