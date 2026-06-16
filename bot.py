1f1e57748b3bd9fddf39264f48856d1bf755d169
karan243coder <152502742+karan243coder@users.noreply.github.com>
2026-06-16 13:22:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,19 +2,23 @@ import os
 import logging
 from config import Config
 from pyrogram import Client as Tech_VJ
+
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-
-if __name__ == "__main__" :
-    # create download directory, if not exist
+if __name__ == "__main__":
     if not os.path.isdir(Config.TECH_VJ_DOWNLOAD_LOCATION):
         os.makedirs(Config.TECH_VJ_DOWNLOAD_LOCATION)
+        
     plugins = dict(root="plugins")
-    VJ = Tech_VJ(name="@VJ_BOTZ",
-    bot_token=Config.TECH_VJ_BOT_TOKEN,
-    api_id=Config.TECH_VJ_API_ID,
-    api_hash=Config.TECH_VJ_API_HASH,
-    plugins=plugins)
+    VJ = Tech_VJ(
+        name=Config.TECH_VJ_SESSION_NAME,
+        bot_token=Config.TECH_VJ_BOT_TOKEN,
+        api_id=Config.TECH_VJ_API_ID,
+        api_hash=Config.TECH_VJ_API_HASH,
+        plugins=plugins
+    )
+    
+    logger.info("Bot Started Successfully!")
     VJ.run()
